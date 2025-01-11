c6b863bc28b2e9370ee607a55575b8ac18c32511
asisr38 <asis.rajthala@gmail.com>
2025-01-11 01:36:05 -0500

    delete unused files

--- lib/constants/team.ts
+++
@@ -1,9 +0,0 @@
-export const teamMembers = [
-  {
-    name: 'MillyNish',
-    title: 'Founder & CEO',
-    specialty: 'Strategic Leadership',
-    image: '/images/team/MF-Logo2.png'
-  },
-  // ... other team members
-] 
