78b75a1222d4a02bc32bc17f40ae6d584e6c0bd6
asisr38 <asis.rajthala@gmail.com>
2025-01-31 17:55:29 -0500

    added analytics and speed insigt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,23 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Montserrat } from 'next/font/google'
-import Header from "@/app/components/Header"
-import Footer from "@/app/components/Footer"
+import "./globals.css";
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import Header from "@/app/components/Header";
+import Footer from "@/app/components/Footer";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const montserrat = Montserrat({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'MillyFam - Financial Success Community',
-  description: 'Join MillyFam\'s exclusive Discord community for crypto plays, sports betting tips, stock insights, and more.',
-}
+  title: "MillyFam - Financial Success Community",
+  description:
+    "Join MillyFam's exclusive Discord community for crypto plays, sports betting tips, stock insights, and more.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -22,8 +25,9 @@ export default function RootLayout({
         <Header />
         <main>{children}</main>
         <Footer />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
-  )
+  );
 }
-
